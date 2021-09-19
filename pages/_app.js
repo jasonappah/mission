e19c21d1cf8489d23cc4c2fb10e37e0d8286b37d
Vitor Vavolizza <vitorvavolizza@gmail.com>
2021-09-19 03:21:22 -0300

    added auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react"
 import '../styles/globals.css'
+import { AuthUserProvider } from '../context/AuthUserContext.js';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <AuthUserProvider>
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
+    </AuthUserProvider>
+
   )
 }
 export default MyApp
